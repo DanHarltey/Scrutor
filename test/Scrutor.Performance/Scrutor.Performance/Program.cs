2c15d9d5f5b83e3923664f29fa5432aef13f441d
Dan H <dan.r.hartley@gmail.com>
2022-07-15 14:43:56 +0100

    Adding perf tests

---
+++ test/Scrutor.Performance/Scrutor.Performance/Program.cs
@@ -0,0 +1,72 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Scrutor.Performance
+{
+    [MemoryDiagnoser]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = BenchmarkRunner.Run(typeof(Program));
+        }
+
+        private ServiceProvider? originalStrategyProvider;
+        private ServiceProvider? proxiedTypeProvider;
+
+        [GlobalSetup]
+        public void SetUp()
+        {
+            ServiceCollectionExtensions.DecorationFactory = new Decoration.Strategies.Original.DecorationFactory();
+
+            originalStrategyProvider = CreateServiceProvider();
+
+            ServiceCollectionExtensions.DecorationFactory = new Decoration.Strategies.ProxiedType.DecorationFactory();
+
+            proxiedTypeProvider = CreateServiceProvider();
+        }
+
+        private static ServiceProvider CreateServiceProvider() => 
+            new ServiceCollection()
+            .AddTransient<IService, SomeRandomService>()
+            .AddTransient<IDecoratedService, Decorated>()
+            .Decorate<IDecoratedService, Decorator>()
+            .BuildServiceProvider(); 
+
+        [Benchmark()]
+        public object OriginalStrategy() => originalStrategyProvider!.GetRequiredService<IDecoratedService>();
+
+        [Benchmark(Baseline = true)]
+        public object ProxiedTypeStrategy() => proxiedTypeProvider!.GetRequiredService<IDecoratedService>();
+
+        public interface IDecoratedService { }
+
+        public interface IService { }
+
+        private class SomeRandomService : IService { }
+
+        public class Decorated : IDecoratedService
+        {
+            public Decorated(IService injectedService)
+            {
+                InjectedService = injectedService;
+            }
+
+            public IService InjectedService { get; }
+        }
+
+        public class Decorator : IDecoratedService
+        {
+            public Decorator(IDecoratedService inner, IService injectedService)
+            {
+                Inner = inner;
+                InjectedService = injectedService;
+            }
+
+            public IDecoratedService Inner { get; }
+
+            public IService InjectedService { get; }
+        }
+    }
+}
